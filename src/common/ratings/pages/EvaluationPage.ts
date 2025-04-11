f8360e81ec5b8ff87fa497f501ac575eca4576ce
Julian Gödde <goedde@tuta.io>
2025-04-11 08:21:53 +0200

    feat: refactor action handlers and improve rating logic
    - Added check for business users. Business users always get to rate the app each 4 months.

--- src/common/ratings/pages/EvaluationPage.ts
+++ src/common/ratings/pages/EvaluationPage.ts
@@ -17,38 +17,47 @@ interface EvaluationPageAttrs {
 }
 
 export class EvaluationPage implements Component<EvaluationPageAttrs> {
-	view({ attrs: { triggerType, dialog, navigate } }: Vnode<EvaluationPageAttrs>): Children {
+	view({ attrs }: Vnode<EvaluationPageAttrs>): Children {
 		return m(ImageWithOptionsDialog, {
 			image: `${window.tutao.appState.prefixWithoutFile}/images/rating/your-opinion-${client.isCalendarApp() ? "calendar" : "mail"}.png`,
 			imageStyle: { "max-width": "300px" },
 			titleText: "ratingHowAreWeDoing_title",
 			messageText: "ratingExplanation_msg",
 			mainActionText: "ratingLoveIt_label",
-			mainActionClick: () => {
-				completeEvaluationStage(triggerType, "LoveIt")
-
-				const lastRatingPromptedDate = deviceConfig.getLastRatingPromptedDate()
-				if (
-					lastRatingPromptedDate == null ||
-					DateTime.fromJSDate(lastRatingPromptedDate).diff(DateTime.fromJSDate(getCurrentDate()), ["years"]).years > 1
-				) {
-					if (isIOSApp()) {
-						deviceConfig.setLastRatingPromptedDate(getCurrentDate())
-						void locator.systemFacade.requestInAppRating()
-						dialog.close()
-					} else {
-						navigate("androidPlayStore")
-					}
-				} else {
-					navigate("supportTuta")
-				}
-			},
+			mainActionClick: () => this.onMainActionClick(attrs),
 			subActionText: "ratingNeedsWork_label",
-			subActionClick: () => {
-				completeEvaluationStage(triggerType, "NeedsWork") //Fixme
-
-				navigate("dissatisfaction")
-			},
+			subActionClick: () => this.onSubActionClick(attrs),
 		})
+	}
+
+	private async onMainActionClick({ dialog, navigate, triggerType }: EvaluationPageAttrs) {
+		completeEvaluationStage(triggerType, "LoveIt")
+
+		const isBusinessUser = (await locator.logins.getUserController().loadCustomer()).businessUse
+
+		const lastRatingPromptedDate = deviceConfig.getLastRatingPromptedDate()
+
+		// Did the user rate within the last year already?
+		const hasRated =
+			lastRatingPromptedDate != null && DateTime.fromJSDate(lastRatingPromptedDate).diff(DateTime.fromJSDate(getCurrentDate()), ["years"]).years <= 1
+
+		if (!hasRated || isBusinessUser) {
+			if (isIOSApp()) {
+				// We cannot get the result of the user rating on iOS, so we have to set the date here.
+				deviceConfig.setLastRatingPromptedDate(getCurrentDate())
+				dialog.close()
+				void locator.systemFacade.requestInAppRating()
+			} else {
+				navigate("androidPlayStore")
+			}
+		} else {
+			navigate("supportTuta")
+		}
+	}
+
+	private onSubActionClick({ triggerType, navigate }: EvaluationPageAttrs) {
+		completeEvaluationStage(triggerType, "NeedsWork") //Fixme
+
+		navigate("dissatisfaction")
 	}
 }
